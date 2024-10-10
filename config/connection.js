3a91c05c735e4dc87a93da1652f0d5650d1ff00d
Brad Kelley <bkfleet1@gmail.com>
2024-10-10 00:15:54 -0500

    NPM Package Update

--- config/connection.js
+++ config/connection.js
@@ -11,16 +11,4 @@ const sequelize = process.env.JAWSDB_URL
     port: 3306
   });
 
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
-
 module.exports = sequelize;
